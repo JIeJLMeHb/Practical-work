9d386ebe31aff92ce80ab59f4489e6c9bdbb5e5d
zloy_pelmenb <GEORGE_HEMINGTON@TUTAMAIL.COM>
2025-08-28 17:13:36 +0300

    lab 3 solution

--- JIeJLMeHb's/lab3/main.py
+++ JIeJLMeHb's/lab3/main.py
@@ -1,7 +1,13 @@
-import csv_module as dt
+import capsule as caps
+import playstation_players as ps
+
 
 def main():
-    dt.main()
+    user_choice = int(input("Для функции логгирования выберите 1, для списка пользователей PS выберите 2: "))
+    if user_choice == 1:
+        caps.hello()
+    if user_choice == 2:
+        ps.main()
 
 if __name__ == "__main__":
     main()
